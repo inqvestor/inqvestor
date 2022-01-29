45f353a44358f71d71407ff4f739ccf24747124f
Gopi Molakalapalli <gopiramana@gmail.com>
2022-01-29 16:59:22 -0500

    Init Git Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { map, mergeMap } from 'rxjs';
+import { ConfigReaderService } from './services/config-reader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'broader';
+  constructor(private configServer: ConfigReaderService) {}
+
+  ngOnInit(): void {
+    // this.configServer.getJSON('../../assets/configs').subscribe( p=> console.log(p['apiServer']));
+    this.configServer
+      .getConfigItem('apiServer')
+      .subscribe((v) => console.log(v));
+
+    const data$ = this.configServer
+      .getConfigItem('apiServer')
+      .pipe(map((apiURL) => this.configServer.getJSON(apiURL)));
+
+    data$.subscribe((data$) => {
+      data$.subscribe((data) =>
+        console.log('-----------111----------------', data)
+      );
+    });
+
+    const data2$ = this.configServer
+      .getConfigItem('apiServer')
+      .pipe(mergeMap((apiURL) => this.configServer.getJSON(apiURL)));
+
+    data2$.subscribe((data2) => {
+      console.log('---------2222------------', data2);
+    });
+  }
 }
--- src/app/services/config-reader.service.ts
+++ src/app/services/config-reader.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, take } from 'rxjs';
+
+export interface configs {
+  apiServer: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConfigReaderService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getJSON(file: string): Observable<any> {
+    return this.http.get(file + '.json');
+  }
+
+  getConfigItem(item: string): Observable<string> {
+    return this.getJSON('../../assets/configs').pipe(
+      map((p) => {
+        return p[item];
+      })
+    );
+  }
 }
